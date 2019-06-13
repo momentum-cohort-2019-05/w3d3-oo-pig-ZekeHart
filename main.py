beeab1fa1eefe749d4e34662e98b28a35260374b
Zeke Hart <heirloch@gmail.com>
2019-06-13 10:03:54 -0400

    small update to player dict generation

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from dice import Dice
 from game_classes import Gamecon, Player
 
+d20_x_1 = Dice(20)
+d5_x_3 = Dice(5,3)
 
+print(d20_x_1, d5_x_3)
 player1 = Player(True)
 player2 = Player()
 test_players_list = [player1, player2]
